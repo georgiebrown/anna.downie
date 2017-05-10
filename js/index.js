9f369407ef69313a76e53b67935b35d9e56fd636
Georgie Brown <georginaabrown@gmail.com>
2017-05-10 23:04:23 +1000

    nnn

--- js/index.js
+++ js/index.js
@@ -17,4 +17,18 @@ function toggleEmail(){
   console.log('clicked');
   $('.hiddenEmail').toggleClass('active');
 }
+
+//getting video element using jQuery
+     var v = $(".video");
+
+  //check if video is ready to play
+     $(v).on('canplay', function(){
+        $(v).mouseenter(function(){
+           $(this).get(0).play();
+        }).mouseleave(function(){
+           $(this).get(0).pause();
+        })
+     });
+
+
 }
